549ad25d2f39893ee0f2a93ba7508e7f2ae212b4
calhoun137 <calhoun137@gmail.com>
2014-08-31 22:54:53 -0400

    formatted example code

--- example.js
+++ example.js
@@ -2,13 +2,24 @@ $(function() {
 
 	var batch = new AsyncBatch;
 
-	var f = function(params) {
+	var asyncFunction = function(params) {
 		setTimeout(params.success, params.timer);
 	}
 	
-	batch.add(f, { success: function() { console.log('a'); }, timer: 3000 });
-	batch.add(f, { success: function() { console.log('b'); }, timer: 2000 });
-	
+	batch.add(asyncFunction, { 
+		success: function() { 
+			console.log('a'); 
+		}, 
+		timer: 3000 
+	});
+
+	batch.add(asyncFunction, { 
+		success: function() { 
+			console.log('b'); 
+		}, 
+		timer: 3000 
+	});
+
 	batch.add($.ajax, { 
 		url: 'http://example.com',
 		type: 'GET',
